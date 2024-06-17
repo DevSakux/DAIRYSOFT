95e3a3da401afc598640b5a68a15220defb61b53
DevSakux <151970112+DevSakux@users.noreply.github.com>
2024-06-17 13:37:25 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Conexión a la base de datos
-$conex = mysqli_connect("localhost", "root", "", "dairysoft");
-
-// Verificar la conexión
-if (!$conex) {
-    die("Conexión fallida: " . mysqli_connect_error());
-}
-?>
